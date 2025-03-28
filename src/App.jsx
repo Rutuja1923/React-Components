60846f990410c94c5f778f66ad36a2f7993fb606
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-28 15:34:56 +0530

    Product details

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-import ProductList from './Components/ProductCard/ProductList';
+import ProdDetails from './Components/ProductCard/ProdDetails';
 
 function App(){
   return <>
-     <ProductList />
+    <ProdDetails title="Phone" description="A sleek and powerful smartphone with an all-day battery." idx={2} image='phone.png'/>
+    <ProdDetails title="Laptop" description="A high-performance laptop for work and entertainment." idx={1} image='laptop.png'/>
+    <ProdDetails title="Mouse" description="A smooth and responsive wireless mouse for effortless navigation." idx={3} image='mouse.png'/>
   </>;
 }
 export default App;
