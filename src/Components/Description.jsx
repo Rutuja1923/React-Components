e98aad40299661117db44f6c8b75ed7b43d0c984
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-28 10:55:38 +0530

    title, description into heading component

---
+++ src/Components/Description.jsx
@@ -0,0 +1,9 @@
+function Description() {
+  return (
+    <>
+        <h3>I am the Description</h3> 
+    </>
+  )
+}
+
+export default Description
---
+++ src/Components/Heading.jsx
@@ -0,0 +1,13 @@
+import Title from './Title.jsx';
+import Description from './Description.jsx'
+
+function Heading(){
+    return (
+        <>
+            <Title />
+            <Description />
+        </>
+    );
+}
+
+export default Heading;
