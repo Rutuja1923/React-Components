c38af32559961c2ba4fd19f36195298c078e6f59
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-29 09:12:40 +0530

    Product Card display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import AmazonList from './Components/ProductCard/AmazonList';
 import ProdDetails from './Components/ProductCard/ProdDetails';
 
 function App(){
-  return <>
-    <ProdDetails title="Phone" description="A sleek and powerful smartphone with an all-day battery." idx={2} image='phone.png'/>
-    <ProdDetails title="Laptop" description="A high-performance laptop for work and entertainment." idx={1} image='laptop.png'/>
-    <ProdDetails title="Mouse" description="A smooth and responsive wireless mouse for effortless navigation." idx={3} image='mouse.png'/>
-  </>;
+  return (
+    <>
+      <ProdDetails title="Phone" description="A sleek and powerful smartphone with an all-day battery." idx={2} image='phone.png' features={["High-resolution", "Long battery life", "Fast charging", "Powerful processor"]}/>
+      <ProdDetails title="Laptop" description="A high-performance laptop for work and entertainment." idx={1} image='laptop.png' features={["High-resolution", "High-speed processor", "Full HD display"]}/>
+      <ProdDetails title="Mouse" description="A smooth and responsive wireless mouse for effortless navigation." idx={3} image='mouse.png' features={["Ergonomic design", "Long battery life", "Wireless connectivity"]}/>  
+      <AmazonList />
+    </>
+  ); 
+  
 }
 export default App;
