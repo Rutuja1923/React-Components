dfedbf9911b2c240ee282763b4b9e7ba10d3e4d3
Rutuja1923 <rutujajirage19@gmail.com>
2025-03-29 10:07:05 +0530

    Rendering of simple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
-import AmazonList from './Components/ProductCard/AmazonList';
-import ProdDetails from './Components/ProductCard/ProdDetails';
+import MsgBox from './Components/MsgBox';
+import Intro from './Components/Intro';
+import OldApp from './Components/OldApp';
+import Compare from './Components/Compare';
+import { PrintInfo } from './Components/PrintInfo';
+import { MathDemo } from './Components/MathDemo';
 
 function App(){
   return (
     <>
-      <ProdDetails title="Phone" description="A sleek and powerful smartphone with an all-day battery." idx={2} image='phone.png' features={["High-resolution", "Long battery life", "Fast charging", "Powerful processor"]}/>
-      <ProdDetails title="Laptop" description="A high-performance laptop for work and entertainment." idx={1} image='laptop.png' features={["High-resolution", "High-speed processor", "Full HD display"]}/>
-      <ProdDetails title="Mouse" description="A smooth and responsive wireless mouse for effortless navigation." idx={3} image='mouse.png' features={["Ergonomic design", "Long battery life", "Wireless connectivity"]}/>  
-      <AmazonList />
+      <MsgBox userName="Reena" textColor="midnightblue"/>
+      <Intro />
+      <OldApp />
+      <Compare />
+      <PrintInfo />
+      <MathDemo />
     </>
   ); 
   
